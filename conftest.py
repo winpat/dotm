e5faf1ceefb5a325bdeb037a7fd00291027b3c14
Patrick Winter <patrick.winter@adfinis-sygroup.ch>
2020-05-24 21:06:00 +0200

    Add minimal test suite

---
+++ conftest.py
@@ -0,0 +1,35 @@
+import pytest
+import yaml
+
+from pathlib import Path
+from os import path
+
+
+@pytest.fixture
+def source_dir(tmp_path):
+    source = tmp_path / "source"
+    source.mkdir()
+    return str(source)
+
+
+@pytest.fixture
+def dest_dir(tmp_path):
+    dest = tmp_path / "destination"
+    dest.mkdir()
+    return str(dest)
+
+
+@pytest.fixture
+def dotrc(source_dir, dest_dir):
+    return {"all": [".emacs", ".tmux.conf"], "host1": [".vimrc"]}
+
+
+def touch_dotrc(source_dir, dotrc):
+    files = flatten(dotrc.values())
+    for f in files:
+        Path(f"{source_dir}/{f}").touch()
+    return files
+
+
+def flatten(l):
+    return [i for sl in l for i in sl]
---
+++ dotm_test.py
@@ -0,0 +1,27 @@
+import pytest
+
+from dotm import load_config, link
+from conftest import flatten, touch_dotrc
+from pathlib import Path
+
+
+def test_missing_dotrc(source_dir, capsys):
+    with pytest.raises(SystemExit) as we:
+        load_config(source_dir)
+        assert we.value.code == 1
+
+    captured = capsys.readouterr()
+    assert captured.out == "Source directory does not contain a .dotrc file\n"
+
+
+def test_link(source_dir, dest_dir, dotrc, mocker):
+
+    files = touch_dotrc(source_dir, dotrc)
+    mocker.patch("dotm.gethostname", return_value="host1")
+
+    link(dotrc, source_dir, dest_dir)
+
+    for f in files:
+        p = Path(f"{dest_dir}/{f}")
+        assert p.is_symlink()
+        assert p.resolve(f"{source_dir}/{f}")
