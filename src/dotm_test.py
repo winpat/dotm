36447a63f66fbdc53e6aaa6dcb1aa44b215e1d5e
Patrick Winter <patrickwinter@posteo.ch>
2020-05-25 19:15:05 +0200

    Add test cases for missing and existing file

--- src/dotm_test.py
+++ src/dotm_test.py
@@ -15,7 +15,19 @@ def test_missing_dotrc(source_dir, capsys):
     assert captured.out == "Source directory does not contain a .dotrc file\n"
 
 
-def test_link(source_dir, dest_dir, dotrc, mocker):
+def test_invalid_dotrc(source_dir, capsys):
+    dotrc = Path(source_dir) / ".dotrc"
+    dotrc.write_text("somekey: somevalue: someothervalue")
+
+    with pytest.raises(SystemExit) as we:
+        load_config(source_dir)
+        assert we.value.code == 1
+
+    captured = capsys.readouterr()
+    assert captured.out == ".dotrc is not valid\n"
+
+
+def test_host_specific_dotrc(source_dir, dest_dir, dotrc, mocker):
 
     files = touch_dotrc(source_dir, dotrc)
     mocker.patch("dotm.gethostname", return_value="host1")
@@ -28,13 +40,26 @@ def test_link(source_dir, dest_dir, dotrc, mocker):
         assert p.resolve(f"{source_dir}/{f}")
 
 
-def test_invalid_dotrc(source_dir, capsys):
-    dotrc = Path(source_dir) / ".dotrc"
-    dotrc.write_text("somekey: somevalue: someothervalue")
+def test_existing_dotfile(source_dir, dest_dir):
+    dotfile = ".emacs"
+    dotrc = {"all": [dotfile]}
+    touch_dotrc(source_dir, dotrc)
 
-    with pytest.raises(SystemExit) as we:
-        load_config(source_dir)
-        assert we.value.code == 1
+    # Ensure the link exists
+    source_file = Path(source_dir, dotfile)
+    link_target = Path(dest_dir, dotfile)
+    link_target.symlink_to(source_file)
 
-    captured = capsys.readouterr()
-    assert captured.out == ".dotrc is not valid\n"
+    existing, missing = link(dotrc, source_dir, dest_dir)
+    assert len(existing) == 1
+    assert len(missing) == 0
+
+
+def test_missing_dotfile(source_dir, dest_dir):
+    dotfile = ".emacs"
+    dotrc = {"all": [dotfile]}
+    touch_dotrc(source_dir, dotrc)
+
+    existing, missing = link(dotrc, source_dir, dest_dir)
+    assert len(missing) == 1
+    assert len(existing) == 0
