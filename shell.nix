01ad84f579f291ede26551434c915ed5a4079a3e
Patrick Winter <patrickwinter@posteo.ch>
2023-01-08 14:56:17 +0100

    Upgrade Python version from 3.7 to 3.10

--- shell.nix
+++ shell.nix
@@ -4,27 +4,16 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "env";
   buildInputs = [
-    python37
-    python37Packages.pyyaml
+    python3
+    python3Packages.pyyaml
 
     # Developement
-    python37Packages.pytest
-    python37Packages.pytest-mock
-    python37Packages.pytest-isort
-    python37Packages.pytest-flake8
-    python37Packages.pytest-black
-    python37Packages.pytest-mypy
-
-    python37Packages.ipython
-    python37Packages.ipdb
-
-    python37Packages.black
-    python37Packages.isort
-    python37Packages.mypy
-
-    python37Packages.python-language-server
-    python37Packages.pyls-black
-    python37Packages.pyls-isort
-    python37Packages.pyls-mypy
+    python3Packages.pytest
+    python3Packages.pytest-mock
+    python3Packages.pytest-isort
+    python3Packages.pytest-flake8
+    python3Packages.pytest-black
+    python3Packages.pytest-mypy
+    python3Packages.ipdb
   ];
 }
